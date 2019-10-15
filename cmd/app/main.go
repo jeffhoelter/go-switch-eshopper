dd79fa60ec132a5547f6a80d72fd17039575ed05
Jeff Hoelter <jeffh@loar.net>
2019-10-15 15:52:11 -0400

    Added initial code, docker configuration, reading environment variables into a config.

---
+++ cmd/app/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"myapp/config"
+)
+
+func main() {
+
+	appConf := config.AppConfig()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Greet)
+
+	address := fmt.Sprintf(":%d", appConf.Server.Port)
+
+	log.Printf("Starting server %s\n", address)
+
+	s := &http.Server{
+		Addr:         address,
+		Handler:      mux,
+		ReadTimeout:  appConf.Server.TimeoutRead,
+		WriteTimeout: appConf.Server.TimeoutWrite,
+		IdleTimeout:  appConf.Server.TimeoutIdle,
+	}
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal("Server startup failed")
+	}
+}
+func Greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jeffhoelter/go-switch-eshopper
+
+go 1.13
+
+require github.com/joeshaw/envdecode v0.0.0-20190604014844-d6d9849fcc2c // indirect
